c1d0ebf3bcd0bcee41a3b67fe49d21c2640aee0b
Pranav Tondgaonkar <pranavrelds@gmail.com>
2023-03-10 03:53:09 +0530

    initial setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prediction",
+    version="0.0.1",
+    author="Pranav Tondgaonkar",
+    author_email="pranavrelds@gmail.com",
+    packages=find_packages()
+)
